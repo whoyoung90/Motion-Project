c6139db84ff02d560da558653f84365ea4437ab8
kimwooyoung <whoyoung90@naver.com>
2023-03-13 23:55:22 +0900

    study: 230313 Page 컴포넌트 만들기 복습 완료

--- src/components/page.ts
+++ src/components/page.ts
@@ -1,5 +1,5 @@
 export class PageComponent {
-  private element: HTMLUListElement;
+  private element: HTMLUListElement; // 카드들의 목록을 담으므로 ul
   constructor() {
     this.element = document.createElement("ul");
     this.element.setAttribute("class", "page");
@@ -7,8 +7,11 @@ export class PageComponent {
   }
 
   /**
+   * 외부에서 사용할 수 있는 attachTo.
    * PageComponent를 만드는 사람이 attachTo를 호출함으로써
-   * 이 페이지를 추가하고 싶은 요소에 전달해주면, 함수가 알아서 페이지를 추가해준다.
+   * 이 페이지에 추가하고 싶은 요소를 전달해주면, 함수 내부적으로 페이지를 추가해준다.
+   *
+   * position에 아무것도 전달하지 않으면 기본적으로 "afterbegin"
    */
   attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
     parent.insertAdjacentElement(position, this.element);
