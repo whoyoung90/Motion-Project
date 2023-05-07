45ed244c63512bafd20cf15bcfec006c26fa0ab4
kimwooyoung <whoyoung90@naver.com>
2023-05-07 23:12:25 +0900

    study: 230507 최종 완료

--- src/decorators/log.ts
+++ src/decorators/log.ts
@@ -1,6 +1,7 @@
 /**
  * @description Decorators
- * 기존의 함수나 클레스를 한 단계 감싸 꾸며줄 수 있는 Wrapper Class를 만들 수 있다
+ * 기존의 함수나 클레스를 한 단계 감싸 꾸며줄 수 있는 Wrapper Class를 만들 수 있다.
+ * "@"와 같은 annotation을 활용하여 우리가 원하는 곳에서 "재사용" 할 수 있다.
  */
 function Log(
   _: any,
